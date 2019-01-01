95d4e1e230e5fccc4c4b2dcb3714f5fcbf930f36
jamesbarboza <jamesbarboza720@gmail.com>
2019-01-02 01:00:21 +0530

    script to store web content (inshorts) added

---
+++ scripts/inshorts_collector.py
@@ -0,0 +1,43 @@
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
+import nltk
+import re
+import requests
+import json
+import os
+import sys
+sys.path.append("..")
+import project_config as config
+os.chdir(config.__training_dir__)
+
+category = "technology"
+domain = "https://inshorts.com"
+url = "https://inshorts.com/en/read/" + category
+
+#	Read the page from the url
+html_page = urlopen(url).read()
+
+#	Use the BeautifulSoup class to arrange and find tags
+soup = BeautifulSoup(html_page, "html.parser")
+
+#	Find all divs with the clas
+divs = soup.findAll("div", { "class" : "news-card" })
+
+folders = os.listdir()
+
+if category not in folders:
+	os.mkdir(category)
+	os.chdir(category)
+else:
+	os.chdir(category)
+
+for div in divs:
+	title = div.find("span", { "itemprop" : "headline"}).text
+	content = div.find("div", { "itemprop" : "articleBody" }).text
+
+	filename = title + ".txt"
+	filecontent = title + "\n\n" + content
+	files = os.listdir()
+	if filename not in files:
+		with open(filename, "w") as file:
+			file.write(filecontent)
--- scripts/twitter/handler.py
+++ scripts/twitter/handler.py
@@ -2,6 +2,7 @@ from tweepy import Stream
 from tweepy import OAuthHandler
 from tweepy.streaming import StreamListener
 import json
+import datetime
 
 import sys
 sys.path.append("../..")
@@ -10,8 +11,14 @@ import project_config as config
 class Listener(StreamListener):
 	def on_data(self, data):
 		tweet_data = json.loads(data)
-		print(tweet_data['text'])
+		data_to_write = tweet_data['text'] + "\n\n"
+		print(data_to_write)
 
+		timestamp = datetime.datetime.now()
+		today = timestamp.strftime('%d%m%y')
+
+		with open(config.__training_dir__ + 'tech_and_startup/twitter' + today +'.txt', 'a') as twitter_file:
+			twitter_file.write(data_to_write)
 
 	def on_error(self, status):
 		print(status)
@@ -20,4 +27,7 @@ auth = OAuthHandler(config.capi, config.capi_secret)
 auth.set_access_token(config.ctoken, config.ctoken_secret)
 
 twitter_stream = Stream(auth, Listener())
-twitter_stream.filter(track=["USA", "Donald Trump", "Mauro Viale", "politics"])
+
+#keywords = ["USA", "Donald Trump", "Mauro Viale", "politics"]
+keywords = [ "tech", "technology", "startup", "innovation"]
+twitter_stream.filter(track=keywords)
