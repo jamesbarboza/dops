92aebde0eae5ef6cc3c8afe6c3e4d0f8aec3e0a1
James Barboza <jamesbarboza720@gmail.com>
2019-02-17 14:33:55 +0530

    classifier class added

---
+++ libs/nlp_engine/classifier.py
@@ -0,0 +1,25 @@
+import pickle
+from nltk.tokenize import sent_tokenize, word_tokenize
+import project_config as config
+
+class MultinomialClassifier():
+
+    def __init__(self):
+        f = open(config.__project_dir__ + "data/nlp_engine/document_classifications/classifiers/multinomial_nb.pkl", "rb")
+        self.classifier = pickle.load(f)
+        f.close()
+    
+    def find_features(self, text):
+        f = open(config.__project_dir__ + "data/nlp_engine/document_classifications/features.pkl", "rb")
+        word_features = pickle.load(f)
+        f.close()
+        features = {}
+        doc_words = word_tokenize(text)
+        for word in word_features:
+            features[word] = (word in doc_words)
+        return features
+    
+    def classify(self, text):
+        feats = self.find_features(text)
+        result = self.classifier.classify(feats)
+        return result
