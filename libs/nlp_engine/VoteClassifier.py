b10c78d5ba32d6b6f0c7805654220d940445987d
James Barboza <jamesbarboza720@gmail.com>
2019-04-16 00:59:12 +0530

    sentiment classifiers added

--- libs/nlp_engine/VoteClassifier.py
+++ libs/nlp_engine/VoteClassifier.py
@@ -2,12 +2,13 @@ from nltk.classify import ClassifierI
 from statistics import mode
 import pickle
 import dops.project_config as config
+import nltk
 
 class VoteClassifier(ClassifierI):
 
     def __init__(self):
         self._classifiers = []
-        
+
         file = open(config.__project_dir__ + "data/nlp_engine/document_classifications/classifiers/naive_bayes.pkl", "rb")
         naive_bayes = pickle.load(file)
         self._classifiers.append(naive_bayes)
@@ -47,7 +48,7 @@ class VoteClassifier(ClassifierI):
         for classifier in self._classifiers:
             vote = classifier.classify(features)
             votes.append(vote)
-        
+
         choice_votes = votes.count(mode(votes))
         conf = choice_votes / len(votes)
         return conf
@@ -59,4 +60,64 @@ class VoteClassifier(ClassifierI):
         features = {}
         for word in word_features:
             features[word] = (word in doc_words)
+        return features
+
+
+class SentimentVoteClassifier(ClassifierI):
+
+    def __init__(self):
+        self._classifiers = []
+
+        file = open(config.__project_dir__ + "data/nlp_engine/sentiment-models/nb", "rb")
+        naive_bayes = pickle.load(file)
+        self._classifiers.append(naive_bayes)
+        file.close
+
+        file = open(config.__project_dir__ + "data/nlp_engine/sentiment-models/multinomial_nb", "rb")
+        multinomial_nb = pickle.load(file)
+        self._classifiers.append(multinomial_nb)
+        file.close()
+
+        file = open(config.__project_dir__ + "data/nlp_engine/sentiment-models/bernoulli_nb", "rb")
+        bernoulli_ng = pickle.load(file)
+        self._classifiers.append(bernoulli_ng)
+        file.close()
+
+        file = open(config.__project_dir__ + "data/nlp_engine/sentiment-models/logistic_regression", "rb")
+        logistic_regression = pickle.load(file)
+        self._classifiers.append(logistic_regression)
+        file.close()
+
+        file = open(config.__project_dir__ + "data/nlp_engine/sentiment-models/sgdc", "rb")
+        sgdc = pickle.load(file)
+        self._classifiers.append(sgdc)
+        file.close()
+
+
+    def classify(self, text):
+        votes = []
+        doc_features = self.find_features(text)
+        for classifier in self._classifiers:
+            vote = classifier.classify(doc_features)
+            votes.append(vote)
+        return mode(votes)
+
+    def confidence(self, features):
+        votes = []
+        for classifier in self._classifiers:
+            vote = classifier.classify(features)
+            votes.append(vote)
+
+        choice_votes = votes.count(mode(votes))
+        conf = choice_votes / len(votes)
+        return conf
+
+    def find_features(self, doc_words):
+        file = open(config.__project_dir__ + "data/nlp_engine/sentiment-models/features", "rb")
+        word_features = pickle.load(file)
+        file.close()
+        words = nltk.word_tokenize(doc_words)
+        features = {}
+        for word in word_features:
+            features[word] = (word in words)
         return features
